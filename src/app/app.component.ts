7af0632a9c6a4d481ca212940d7ee4db12434f43
harinishree564 <Harinims564@gmail.com>
2021-12-08 22:59:41 +0530

    myproject

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { getLocaleDayNames } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+
+name="harini";
+getName(){
+  return "tom";
+}
+arr=[1,2,3];
+}
